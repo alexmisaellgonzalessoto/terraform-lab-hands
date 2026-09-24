4267b576e314c09c4383b816462be1c043317140
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-09-24 09:02:58 -0500

    feat: agrega el backend DEV con Node.js

--- main.tf
+++ main.tf
@@ -10,13 +10,16 @@ terraform {
 provider "docker" {
   context = "desktop-linux"
 }
+
 resource "docker_image" "nginx" {
   name = "nginx:1.31.6"
 }
+
 resource "docker_network" "dev" {
   name   = "lab-dev"
   driver = "bridge"
 }
+
 resource "docker_container" "web_dev" {
   name  = "web-dev"
   image = docker_image.nginx.image_id
@@ -35,6 +38,30 @@ resource "docker_container" "web_dev" {
     file    = "/usr/share/nginx/html/index.html"
   }
 }
+
 resource "docker_image" "node" {
   name = "node:24-alpine"
+}
+
+resource "docker_container" "api_dev" {
+  name  = "api-dev"
+  image = docker_image.node.image_id
+
+  ports {
+    internal = 3000
+    external = 4002
+  }
+
+  networks_advanced {
+    name = docker_network.dev.name
+  }
+
+  upload {
+    content = file("${path.module}/backend/index.js")
+    file    = "/app/index.js"
+  }
+
+  working_dir = "/app"
+
+  command = ["node", "index.js"]
 }
