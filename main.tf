975a079161e58ab31ef7646b3360d7469681d458
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-09-24 08:30:59 -0500

    feat: agrega el frontend DEV con Nginx

--- main.tf
+++ main.tf
@@ -17,3 +17,16 @@ resource "docker_network" "dev" {
   name   = "lab-dev"
   driver = "bridge"
 }
+resource "docker_container" "web_dev" {
+  name  = "web-dev"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 4001
+  }
+
+  networks_advanced {
+    name = docker_network.dev.name
+  }
+}
